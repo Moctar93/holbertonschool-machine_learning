0fcb9dd6df7f65fef65862940bf6d73cacc227a3
Moctar93 <moctartize93@gmail.com>
2024-12-13 17:12:40 +0100

    Line Graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-import matplotlib
-matplotlib.use('TkAgg')  # Set an interactive backend
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+    Function that generates a plot and
+    displays it as a red solid line
+    """
+
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 11)
-    plt.plot(x, y, 'r-')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Plot of y = x^3')
+
+    plt.plot(y, 'r-')
+    plt.xlim(0, 10)
     plt.show()
-
-line()
-
