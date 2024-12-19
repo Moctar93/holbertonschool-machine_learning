ee46ff78addfa456d11764676cc5dc050e3c78cb
Moctar93 <moctartize93@gmail.com>
2024-12-19 03:24:11 +0100

    update files

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -19,9 +21,12 @@ class Node:
         """
         if self.is_leaf:
             return self.depth
-        left_depth = self.left_child.max_depth_below() if self.left_child else 0
-        right_depth = self.right_child.max_depth_below() if self.right_child else 0
-        return max(left_depth, right_depth)
+        left_depth = self.left_child.max_depth_below() if self.left_child
+    else 0
+    right_depth = self.right_child.max_depth_below() if self.right_child
+    else 0
+    return max(left_depth, right_depth)
+
 
 class Leaf(Node):
     def __init__(self, value, depth=None):
@@ -36,8 +41,10 @@ class Leaf(Node):
         """
         return self.depth
 
+
 class Decision_Tree:
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -55,4 +62,3 @@ class Decision_Tree:
         Retourne la profondeur maximale de l'arbre de décision.
         """
         return self.root.max_depth_below()
-
