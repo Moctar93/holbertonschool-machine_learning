cb033f1abbc5aa1b8f8b5e94b5bff8bb7e22743a
Moctar93 <moctartize93@gmail.com>
2024-12-20 16:15:33 +0100

    Towards the predict function (1) : the get_leaves method

--- supervised_learning/decision_tree/3-build_decision_tree.py
+++ supervised_learning/decision_tree/3-build_decision_tree.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Node:
+    """
+    Représente un nœud interne dans un arbre de décision.
+
+    Attributs :
+    ----------
+    - feature : int
+        L'indice de la caractéristique utilisée pour diviser.
+    - threshold : float
+        Le seuil utilisé pour diviser les données.
+    - left_child : Node ou Leaf
+        Le sous-arbre gauche ou feuille associé à ce nœud.
+    - right_child : Node ou Leaf
+        Le sous-arbre droit ou feuille associé à ce nœud.
+    - is_root : bool
+        Indique si ce nœud est la racine de l'arbre.
+    - depth : int
+        La profondeur du nœud dans l'arbre.
+    - is_leaf : bool
+        Toujours `False` pour un nœud interne (contrairement à une feuille).
+    """
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         self.feature = feature
