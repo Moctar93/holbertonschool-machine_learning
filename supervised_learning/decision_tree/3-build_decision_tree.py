e4f9980bbd38d1ac244f2dbfe3879a08cac80531
Moctar93 <moctartize93@gmail.com>
2024-12-20 16:00:47 +0100

    Towards the predict function (1) : the get_leaves method

---
+++ supervised_learning/decision_tree/3-build_decision_tree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_root = is_root
+        self.depth = depth
+        self.is_leaf = False
+
+    def get_leaves_below(self):
+        """Returns the list of all leaves of the tree below this node."""
+        leaves = []
+        if self.left_child:
+            leaves.extend(self.left_child.get_leaves_below())
+        if self.right_child:
+            leaves.extend(self.right_child.get_leaves_below())
+        return leaves
+
+
+class Leaf:
+    def __init__(self, value, depth=0):
+        self.value = value
+        self.depth = depth
+        self.is_leaf = True
+
+    def get_leaves_below(self):
+        """Returns the list of all leaves (itself, as it is a leaf)."""
+        return [self]
+
+    def __repr__(self):
+        return f"-> leaf [value={self.value}]"
+
+
+class Decision_Tree:
+    def __init__(self, root):
+        self.root = root
+
+    def get_leaves(self):
+        """Returns the list of all leaves of the decision tree."""
+        return self.root.get_leaves_below()
+
