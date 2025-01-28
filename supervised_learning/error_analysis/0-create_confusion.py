d73eac559fd3fc5dac20d851b2e646fa267bdb98
Moctar93 <moctartize93@gmail.com>
2025-01-28 17:41:56 +0100

    Create Confusion

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -2,13 +2,17 @@
 
 import numpy as np
 
+
 def create_confusion_matrix(labels, logits):
     """
-    Crée une matrice de confusion à partir des labels réels et des logits prédits.
+    Crée une matrice de confusion à partir des labels réels et des
+    logits prédits.
 
     Parameters:
-    - labels : numpy.ndarray de forme (m, classes), les labels réels en one-hot encoding
-    - logits : numpy.ndarray de forme (m, classes), les prédictions en one-hot encoding
+    - labels : numpy.ndarray de forme (m, classes), les labels réels en
+    one-hot encoding
+    - logits : numpy.ndarray de forme (m, classes), les prédictions
+    en one-hot encoding
 
     Returns:
     - numpy.ndarray de forme (classes, classes), la matrice de confusion
@@ -18,10 +22,10 @@ def create_confusion_matrix(labels, logits):
     predicted_classes = np.argmax(logits, axis=1)
 
     # Créer la matrice de confusion avec le type float
-    confusion_matrix = np.zeros((labels.shape[1], labels.shape[1]), dtype=float)
+    confusion_matrix = np.zeros((labels.shape[1],)*2, dtype=float)
 
     # Remplir la matrice de confusion
     for true, pred in zip(true_classes, predicted_classes):
-        confusion_matrix[true, pred] += 1.0  # Assurez-vous que les valeurs sont flottantes
+        confusion_matrix[true, pred] += 1.0
 
     return confusion_matrix
