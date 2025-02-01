67fcaf92b92a389556b856606a044580cd8b0d39
Moctar93 <moctartize93@gmail.com>
2025-02-01 19:30:59 +0100

    update task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """
-Creates a tensorflow layer with L2 regularization
+Create a Layer with L2 Regularization.
 """
+
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    a function that creates a tensrflow layer with L2 regularization
-    :param prev: is a tensor containing the output of the previous layer
-    :param n: the number of nodes the new layer should contain
-    :param activation: the activation function that should be used on the layer
-    :param lambtha: the L2 regularization parameter
-    :return: the output of the new layer
+    Create a Layer with L2 Regularization.
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    reg = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init,
-                            name="layer", kernel_regularizer=reg)
+    init_weights = tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode="fan_avg"
+            )
+    l2_regularizer = tf.keras.regularizers.L2(lambtha)
+
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init_weights,
+        kernel_regularizer=l2_regularizer
+    )
+
     return layer(prev)
