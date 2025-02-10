8b0c4df877f861040ddf31a9be6ade0019c4eff7
Moctar93 <moctartize93@gmail.com>
2025-02-10 02:40:01 +0100

    Convolution with Padding

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 """
-performing a same convolution on grayscale images.
+Performs a same convolution on grayscale images
 """
 import numpy as np
 
 
-def convolve_grayscale_same(images, kernel):
+def convolve_grayscale_padding(images, kernel, padding):
     """
-    performing a same convolution on grayscale images.
+    a function that performs a same convolution
+    :param images: a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+    :param kernel: a numpy.ndarray with shape (kh, kw) containing the kernel
+    for the convolution
+        kh is the height of the kernel
+        kw is the width of the kernel
+    :param padding: a tuple of (ph, pw)
+        ph is the padding for the height of the image
+        pw is the padding for the width of the image
+    :return: numpy.ndarray containing the convolved images
     """
-    m, h, w = images.shape
-    kh, kw = kernel.shape
+    c_images = images.shape[0]
+    f_height = kernel.shape[0]
+    f_width = kernel.shape[1]
 
-    convolved_output = np.zeros((m, h, w))
+    padding_h, padding_w = padding
 
-    pad_width = ((0, 0), (kh // 2, kh // 2), (kw // 2, kw // 2))
-    padded_images = np.pad(
-            images, pad_width=pad_width, mode="constant", constant_values=0
-            )
-
-    for i in range(h):
-        for j in range(w):
-            region = padded_images[:, i:(i + kh), j:(j + kw)]
-            convolved_output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
-
-    return convolved_output
+    c_height = images.shape[1] + 2 * padding_h - f_height + 1
+    c_width = images.shape[2] + 2 * padding_w - f_width + 1
+    # np.pad works with a before_N and after_N parameter defined in a tuple
+    # that will add the selected pad at each dimension
+    pad_images = np.pad(images, ((0, 0), (padding_h, padding_h), (padding_w,
+                                                                  padding_w)))
+    convolved = np.zeros((c_images, c_height, c_width))
+    for row in range(c_height):
+        for col in range(c_width):
+            mul_ele = pad_images[:, row:row + f_height, col:col + f_width] * \
+                      kernel
+            sum_ele = np.sum(mul_ele, axis=(1, 2))
+            convolved[:, row, col] = sum_ele
+    return convolved
