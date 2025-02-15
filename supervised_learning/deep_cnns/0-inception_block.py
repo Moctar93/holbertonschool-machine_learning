220ac61a2366ee0e138552e32c53bbd01bca2d7c
Moctar93 <moctartize93@gmail.com>
2025-02-15 08:53:34 +0100

    Inception Block

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -2,58 +2,66 @@
 """
 Inception Block
 """
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def inception_block(A_prev, filters):
     """
-    function that builds an inception block
-    as described in Going Deeper with Convolutions (2014)
-    """
-    initializer = K.initializers.he_normal()
-    F1_layer = K.layers.Conv2D(filters=filters[0],
-                               kernel_size=1,
-                               padding='same',
-                               kernel_initializer=initializer,
-                               activation='relu')
-    F1_output = F1_layer(A_prev)
-    F3R_layer = K.layers.Conv2D(filters=filters[1],
-                                kernel_size=1,
-                                padding='same',
-                                kernel_initializer=initializer,
-                                activation='relu')
-    F3R_output = F3R_layer(A_prev)
-    F3_layer = K.layers.Conv2D(filters=filters[2],
-                               kernel_size=3,
-                               padding='same',
-                               kernel_initializer=initializer,
-                               activation='relu')
-    F3_output = F3_layer(F3R_output)
-    F5R_layer = K.layers.Conv2D(filters=filters[3],
-                                kernel_size=1,
-                                padding='same',
-                                kernel_initializer=initializer,
-                                activation='relu')
-    F5R_output = F5R_layer(A_prev)
-    F5_layer = K.layers.Conv2D(filters=filters[4],
-                               kernel_size=5,
-                               padding='same',
-                               kernel_initializer=initializer,
-                               activation='relu')
-    F5_output = F5_layer(F5R_output)
-    Pool_layer = K.layers.MaxPool2D(pool_size=3,
-                                    padding='same',
-                                    strides=1)
-    Pool_output = Pool_layer(A_prev)
-    FPP_layer = K.layers.Conv2D(filters=filters[5],
-                                kernel_size=1,
-                                padding='same',
-                                kernel_initializer=initializer,
-                                activation='relu')
-    FPP_output = FPP_layer(Pool_output)
+    Function that builds an inception block as described in Going Deeper with Convolutions (2014).
 
-    # concatenate the outputs of the branches
-    output = K.layers.concatenate(
-        [F1_output, F3_output, F5_output, FPP_output])
+    Arguments:
+    A_prev -- input tensor from the previous layer
+    filters -- tuple or list of 6 integers, specifying the number of filters in each convolution:
+               [F1, F3R, F3, F5R, F5, FPP]
+
+    Returns:
+    output -- concatenated output of the inception block
+    """
+    # Initializer
+    initializer = K.initializers.HeNormal()
+
+    # 1x1 Convolution Branch
+    F1 = K.layers.Conv2D(filters=filters[0],
+                         kernel_size=1,
+                         padding='same',
+                         kernel_initializer=initializer,
+                         activation='relu')(A_prev)
+
+    # 3x3 Convolution Branch
+    F3R = K.layers.Conv2D(filters=filters[1],
+                          kernel_size=1,
+                          padding='same',
+                          kernel_initializer=initializer,
+                          activation='relu')(A_prev)
+    F3 = K.layers.Conv2D(filters=filters[2],
+                         kernel_size=3,
+                         padding='same',
+                         kernel_initializer=initializer,
+                         activation='relu')(F3R)
+
+    # 5x5 Convolution Branch
+    F5R = K.layers.Conv2D(filters=filters[3],
+                          kernel_size=1,
+                          padding='same',
+                          kernel_initializer=initializer,
+                          activation='relu')(A_prev)
+    F5 = K.layers.Conv2D(filters=filters[4],
+                         kernel_size=5,
+                         padding='same',
+                         kernel_initializer=initializer,
+                         activation='relu')(F5R)
+
+    # Max-Pooling Branch
+    Pool = K.layers.MaxPool2D(pool_size=3,
+                              strides=1,
+                              padding='same')(A_prev)
+    FPP = K.layers.Conv2D(filters=filters[5],
+                          kernel_size=1,
+                          padding='same',
+                          kernel_initializer=initializer,
+                          activation='relu')(Pool)
+
+    # Concatenate all branches
+    output = K.layers.concatenate([F1, F3, F5, FPP])
 
     return output
