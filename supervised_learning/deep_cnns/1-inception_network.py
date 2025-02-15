556145da9988955cbb923ed7f01f5db5f7f62b1f
Moctar93 <moctartize93@gmail.com>
2025-02-15 09:22:42 +0100

    Inception Network

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -8,7 +8,8 @@ inception_block = __import__('0-inception_block').inception_block
 
 def inception_network():
     """
-    Function that builds the inception network as described in Going Deeper with Convolutions (2014).
+    Function that builds the inception network as described in Going 
+    Deeper with Convolutions (2014).
 
     Returns:
     model -- the Keras model
