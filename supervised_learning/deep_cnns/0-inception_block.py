0a20634b162e042eb6548a320e44048abdff749c
Moctar93 <moctartize93@gmail.com>
2025-02-15 08:22:31 +0100

    Inception Block

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,57 +1,59 @@
 #!/usr/bin/env python3
 """
-   Module contains
-   inception_block(A_prev, filters):
+Inception Block
 """
-
-
 import tensorflow.keras as K
 
 
 def inception_block(A_prev, filters):
     """
-    Builds an inception block as described in
-      Going Deeper with Convolutions (2014)
-
-    Args:
-      A_prev: output from previous layer
-      filter: tuple containing filter dims for each layer
-
-    Return:
-      Concatenated output of inception block
+    function that builds an inception block
+    as described in Going Deeper with Convolutions (2014)
     """
-    F1, F3R, F3, F5R, F5, FPP = filters
-    init = K.initializers.he_normal()
+    initializer = K.initializers.he_normal()
+    F1_layer = K.layers.Conv2D(filters=filters[0],
+                               kernel_size=1,
+                               padding='same',
+                               kernel_initializer=initializer,
+                               activation='relu')
+    F1_output = F1_layer(A_prev)
+    F3R_layer = K.layers.Conv2D(filters=filters[1],
+                                kernel_size=1,
+                                padding='same',
+                                kernel_initializer=initializer,
+                                activation='relu')
+    F3R_output = F3R_layer(A_prev)
+    F3_layer = K.layers.Conv2D(filters=filters[2],
+                               kernel_size=3,
+                               padding='same',
+                               kernel_initializer=initializer,
+                               activation='relu')
+    F3_output = F3_layer(F3R_output)
+    F5R_layer = K.layers.Conv2D(filters=filters[3],
+                                kernel_size=1,
+                                padding='same',
+                                kernel_initializer=initializer,
+                                activation='relu')
+    F5R_output = F5R_layer(A_prev)
+    F5_layer = K.layers.Conv2D(filters=filters[4],
+                               kernel_size=5,
+                               padding='same',
+                               kernel_initializer=initializer,
+                               activation='relu')
+    F5_output = F5_layer(F5R_output)
+    Pool_layer = K.layers.MaxPool2D(pool_size=3,
+                                    padding='same',
+                                    strides=1)
+    Pool_output = Pool_layer(A_prev)
+    FPP_layer = K.layers.Conv2D(filters=filters[5],
+                                kernel_size=1,
+                                padding='same',
+                                kernel_initializer=initializer,
+                                activation='relu')
+    FPP_output = FPP_layer(Pool_output)
 
-    # 1D
-    conv1D = K.layers.Conv2D(
-        F1, (1, 1), padding='same', activation='relu', kernel_initializer=init
-    )(A_prev)
+    # concatenate the outputs of the branches
+    output = K.layers.concatenate(
+        [F1_output, F3_output, F5_output, FPP_output])
 
-    # 1D -> 3x3
-    conv3_1D = K.layers.Conv2D(
-        F3R, (1, 1), padding='same', activation='relu', kernel_initializer=init
-    )(A_prev)
-    conv3 = K.layers.Conv2D(
-        F3, (3, 3), padding='same', activation='relu', kernel_initializer=init
-    )(conv3_1D)
-
-    # 1D -> 5x5
-    conv5_1D = K.layers.Conv2D(
-        F5R, (1, 1), padding='same', activation='relu', kernel_initializer=init
-    )(A_prev)
-    conv5 = K.layers.Conv2D(
-        F5, (5, 5), padding='same', activation='relu', kernel_initializer=init
-    )(conv5_1D)
-
-    # 3x3 pool -> 1D
-    pool = K.layers.MaxPooling2D(
-        (2, 2), strides=(1, 1), padding='same'
-    )(A_prev)
-    convPool = K.layers.Conv2D(
-        FPP, (1, 1), padding='same', activation='relu', kernel_initializer=init
-    )(pool)
-
-    out = K.layers.concatenate([conv1D, conv3, conv5, convPool])
-
-    return out
+    return output
