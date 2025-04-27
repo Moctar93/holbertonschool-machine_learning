d6778744eba7d7d46339701ee9cbb6355ec3712d
Moctar93 <moctartize93@gmail.com>
2025-04-27 21:14:37 +0200

    Sparse Autoencoder

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Implementat a sparse autoencoder model.
+"""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """
+    Creatinga  sparse autoencoder.
+    """
+    inputs = keras.Input(shape=(input_dims,))
+    encoded = inputs
+    for units in hidden_layers:
+        encoded = keras.layers.Dense(units, activation='relu')(encoded)
+    latent = keras.layers.Dense(latent_dims, activation='relu',
+                                activity_regularizer=keras.regularizers.l1(
+                                    lambtha))(encoded)
+
+    decoded_input = keras.Input(shape=(latent_dims,))
+    decoded = decoded_input
+    for units in reversed(hidden_layers):
+        decoded = keras.layers.Dense(units, activation='relu')(decoded)
+    outputs = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+
+    encoder = keras.Model(inputs, latent, name='encoder')
+    decoder = keras.Model(decoded_input, outputs, name='decoder')
+    auto = keras.Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
