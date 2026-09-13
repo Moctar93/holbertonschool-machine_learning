882f298069aae8dae7b4f595fe2d8425d7552f2b
Moctar93 <moctartize93@gmail.com>
2026-09-13 19:40:53 +0200

    hue

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-  """
-  Module for changing the hue of an image.
-  """
+import tensorflow as tf
 
-  import tensorflow as tf
+"""
+Module for changing the hue of an image.
+"""
 
 
-  def change_hue(image, delta):
-      """
-          Changes the hue of an image.
+def change_hue(image, delta):
+    """
+    Changes the hue of an image.
 
-              Args:
-                      image: A 3D tf.Tensor containing the image.
-                              delta: The amount by which to change the hue.
+    Args:
+        image: A 3D tf.Tensor containing the image.
+        delta: The amount by which to change the hue.
 
-                                  Returns:
-                                          The hue-adjusted image.
-                                              """
-      return tf.image.adjust_hue(image, delta)
+    Returns:
+        The hue-adjusted image.
+    """
+    return tf.image.adjust_hue(image, delta)
