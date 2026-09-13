e00217c1061f759d5790fbdab8533992a77832da
Moctar93 <moctartize93@gmail.com>
2026-09-13 17:01:17 +0200

    cancat

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module for concatenating two DataFrames.
+"""
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Concatenates selected Bitstamp data with Coinbase data.
+
+    Args:
+        df1: pandas DataFrame containing Coinbase data.
+        df2: pandas DataFrame containing Bitstamp data.
+
+    Returns:
+        The concatenated pandas DataFrame.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2.loc[:1417411920]
+
+    return pd.concat(
+        [df2, df1],
+        keys=["bitstamp", "coinbase"]
+    )
